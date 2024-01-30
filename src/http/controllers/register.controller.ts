67d9f52c91e05e9d044e217237789f65aa6bcb45
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-01-30 19:09:13 -0300

    feat(controllers): :sparkles: creates and registers User registration controller

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists-error";
+import { RegisterUserService } from "@/services/register";
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<never> {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const registerUserService = new RegisterUserService(usersRepository);
+
+    await registerUserService.execute({ name, email, password });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error;
+  }
+
+  return reply.status(201).send();
+}
