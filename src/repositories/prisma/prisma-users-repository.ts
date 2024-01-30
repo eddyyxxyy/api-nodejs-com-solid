29f23d0c5b1a21302b3cb17c0780d76d8b1a79a0
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-01-30 19:05:29 -0300

    feat(repos): :card_file_box: creates Prisma repo for handling User related db operations

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma } from "@prisma/client";
+
+import { prisma } from "@/lib/prisma";
+
+import { IUsersRepository } from "../users-repository";
+
+export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
