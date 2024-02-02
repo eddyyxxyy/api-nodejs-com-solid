5cc60c8bf343020abb85f4e7cb092b13d58cbc73
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-02-02 15:49:01 -0300

    test(services): :white_check_mark: uses fake repository to check if user's password is being hashed

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from "vitest";
+import { compare } from "bcrypt";
+import { describe, expect, it } from "vitest";
 
-test("check if it works", () => {
-  expect(1 + 1).toBe(2);
+import { RegisterUserService } from "./register";
+
+describe("Register Service", () => {
+  it("should hash user's password upon registration", async () => {
+    const registerService = new RegisterUserService({
+      async create(data) {
+        return {
+          id: "user-1",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+      async findByEmail() {
+        return null;
+      },
+    });
+
+    const { user } = await registerService.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
