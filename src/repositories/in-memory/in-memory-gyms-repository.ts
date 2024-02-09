152d8201c6f4771bb4202da21b018be66bd51428
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-02-09 10:28:01 -0300

    feat(repos): :card_file_box: defines and implements in memory gym registration

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,9 +1,25 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
+import { randomUUID } from "crypto";
 
 import { IGymsRepository } from "../gyms-repository";
 
 export class InMemoryGymsRepository implements IGymsRepository {
   public gyms: Gym[] = [];
+
+  async create(data: Gym) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    };
+
+    this.gyms.push(gym);
+
+    return gym;
+  }
 
   async findById(id: string) {
     const gym = this.gyms.find((gym) => gym.id === id);
