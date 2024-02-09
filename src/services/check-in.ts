70921d13ba7f55bff65e61987c332694a40f8ba8
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-02-09 09:27:45 -0300

    feat(services): :sparkles: uses distance calc to check if users are 100+ meters away from gym

--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -1,10 +1,16 @@
 import { CheckIn } from "@prisma/client";
 
 import type { ICheckInsRepository } from "@/repositories/check-ins-repository";
+import { IGymsRepository } from "@/repositories/gyms-repository";
+import { getDistanceBetweenCordinates } from "@/utils/get-distance-between-cordinates";
+
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface ICheckInsServiceRequest {
   gymId: string;
   userId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface ICheckInsServiceResponse {
@@ -12,12 +18,40 @@ interface ICheckInsServiceResponse {
 }
 
 export class CheckInsService {
-  constructor(private readonly checkInRepository: ICheckInsRepository) {}
+  constructor(
+    private readonly checkInRepository: ICheckInsRepository,
+    private readonly gymsRepository: IGymsRepository,
+  ) {}
 
   async execute({
     gymId,
     userId,
+    userLatitude,
+    userLongitude,
   }: ICheckInsServiceRequest): Promise<ICheckInsServiceResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    const distance = getDistanceBetweenCordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error();
+    }
+
     const checkInOnSameDay = await this.checkInRepository.findByUserIdOnDate(
       userId,
       new Date(),
