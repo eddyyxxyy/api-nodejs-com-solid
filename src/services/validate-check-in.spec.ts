ac589d859aa3cad35b099807f576b5cf08fb02a1
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-02-14 16:42:44 -0300

    test(services): :white_check_mark: assures that check-in validation works properly

---
+++ src/services/validate-check-in.spec.ts
@@ -0,0 +1,65 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { ValidateCheckInsService } from "./validate-check-in";
+
+let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
+let sut: ValidateCheckInsService;
+
+describe("Validate Check In Service", () => {
+  beforeEach(async () => {
+    inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
+    sut = new ValidateCheckInsService(inMemoryCheckInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should be able to validate the check-in", async () => {
+    const createdCheckIn = await inMemoryCheckInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    });
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date));
+    expect(inMemoryCheckInsRepository.checkIns[0].validated_at).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it("shouldn't be able to validate inexistent check-in", async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: "inexistent-id",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await inMemoryCheckInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
